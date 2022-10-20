dc501c282a7bb7325a20582d1993b58e706435a7
EC2 Default User <ec2-user@ip-172-31-6-20.ap-northeast-1.compute.internal>
2022-10-20 13:29:32 +0000

    [Update] 模範解答に合わせ非同期通信を修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,15 @@
 class FavoritesController < ApplicationController
   def create
-    @book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: @book.id)
-    favorite.save
+    book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.new(book_id: book.id)
+    @favorite.save
+    render 'replace_btn'
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: @book.id)
-    favorite.destroy
+    book = Book.find(params[:book_id])
+    @favorite = current_user.favorites.find_by(book_id: book.id)
+    @favorite.destroy
+    render 'replace_btn'
   end
 end
